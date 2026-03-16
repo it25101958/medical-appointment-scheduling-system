1bc042c2567e385bf16a9fad50d2ca503c9cbac1
it25101958 <it25101958@my.sliit.lk>
2026-03-16 22:27:18 +0530

    refactor: import correct package

--- backend/src/main/java/com/medical/app/medical_scheduler/StartupLogger.java
+++ backend/src/main/java/com/medical/app/medical_scheduler/StartupLogger.java
@@ -1,4 +1,4 @@
-package com.project.back_end;
+package com.medical.app.medical_scheduler;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
