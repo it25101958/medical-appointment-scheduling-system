e13a016ec32b17c74eae7abb53c90b9448e87e06
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-04-16 06:39:21 +0530

    feat: add Dashboard component with Portal integration

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Portal from "@/components/admin/Dashboard";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Portal />
+    </>
+  );
+};
+
+export default Dashboard;
